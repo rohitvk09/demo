9d3a544cc8f15e514890307a3f10d4e29562c6d2
rohitvk09 <justlogin49@gmail.com>
2025-04-22 09:50:43 +0530

    Saved

--- demo.java
+++ demo.java
@@ -1,7 +1,8 @@
-package Devops.demo;
+package Devops.Demo;
 
 public class demo {
         public static void main(String[] args) {
             System.out.println("Main");
+            System.out.println("Master");
         }
 }
